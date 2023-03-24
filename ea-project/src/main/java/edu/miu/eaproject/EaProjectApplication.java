d62da40cd0781281ee3e0032ebe8d374a181dca6
Karna Bahadur Shrestha <615216@cs.mum.edu>
2023-03-23 20:23:51 -0500

    work added

--- ea-project/src/main/java/edu/miu/eaproject/EaProjectApplication.java
+++ ea-project/src/main/java/edu/miu/eaproject/EaProjectApplication.java
@@ -23,6 +23,6 @@ public class EaProjectApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-	//test for deploy
+	//test for deploy hjgj
 	}
 }
